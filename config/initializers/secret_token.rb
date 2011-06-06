037e0feaa32aa143654de150b97e1299526e33d5
Ben <benbruscella@gmail.com>
2011-06-06 23:13:52 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpenseDaddy::Application.config.secret_token = 'fba27e6c1e79c05f49eb8e9b1160658abddbc4c18efe421ef6befb9f99b163b71886f0ed618054f32a2c1837e5b0742816f2f873549c45246686bfbc9c994763'
