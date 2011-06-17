8dd49216f8d919360fe7056c2e998dc3cee6e097
Ben <benbruscella@gmail.com>
2011-06-17 22:28:28 +1000

    Accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110615142616) do
+ActiveRecord::Schema.define(:version => 20110617121011) do
+
+  create_table "accounts", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", :force => true do |t|
     t.string   "name"
