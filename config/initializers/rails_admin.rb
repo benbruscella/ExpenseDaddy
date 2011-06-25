d40efe27468943f99802f0f8bd1686da85275b62
Ben <benbruscella@gmail.com>
2011-06-25 20:41:12 +1000

    updated admin settings

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,6 +8,17 @@ RailsAdmin.config do |config|
     object_label_method do
       :custom_label_method
     end
+    list do
+      field :user_id do
+        label "Who"
+      end
+      field :amount
+      field :transaction_type_id do
+        label "Type"
+      end      
+      field :category_id
+      field :spent_at
+    end
   end
   config.model TransactionType do
     object_label_method do
